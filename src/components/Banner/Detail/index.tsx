a04294534dd51d7e1477418016bcf0c7490cb72b
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-01-11 19:23:51 -0300

    fix: responsive of the BannerDetail

--- src/components/Banner/Detail/index.tsx
+++ src/components/Banner/Detail/index.tsx
@@ -1,4 +1,10 @@
-import { Box, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Heading,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 
 type BannerDetailProps = {
   image: string;
@@ -6,6 +12,11 @@ type BannerDetailProps = {
 };
 
 export function BannerDetail({ image, title }: BannerDetailProps) {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box
       w="100%"
@@ -16,17 +27,27 @@ export function BannerDetail({ image, title }: BannerDetailProps) {
       bgSize="cover"
       position="relative"
     >
-      <Heading
-        as="h3"
-        color="gray.50"
-        fontSize="48px"
-        fontWeight="700"
-        position="absolute"
-        bottom="80px"
-        left="200px"
-      >
-        {title}
-      </Heading>
+      <Box maxW="1600px" h="100%" mx="auto" px={10} position="relative">
+        {isWideVersion ? (
+          <Heading
+            as="h1"
+            fontWeight="600"
+            fontSize="5xl"
+            color="gray.50"
+            position="absolute"
+            bottom="60px"
+            textTransform="capitalize"
+          >
+            {title}
+          </Heading>
+        ) : (
+          <Center w="100%" h="100%">
+            <Text as="h1" fontWeight="600" fontSize="1.75rem" color="gray.50">
+              {title}
+            </Text>
+          </Center>
+        )}
+      </Box>
     </Box>
   );
 }
